8c90c6a594b6127771676f1374a7add997f2ddf5
tonypera <paulurbian@hotmail.com>
2019-02-13 12:33:23 -0500

    add views index

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const image = require('../controllers/image');
 module.exports = app => {
     router.get('/', home.index);
     router.get('/images/:image_id', image.index);
-    router.post('/image', image.create);
+    router.post('/images', image.create);
     router.post('/images/:image_id/like', image.like);
     router.post('/images/:image_id/comment', image.comment);
     router.delete('/images/:image_id', image.remove);
