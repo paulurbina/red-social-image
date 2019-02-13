49ee11bd63095cce6609ac3de1980f3287a413ff
tonypera <paulurbian@hotmail.com>
2019-02-13 12:35:42 -0500

    pdate

---
+++ src/controllers/image.js
@@ -0,0 +1,20 @@
+const ctrl = {};
+
+ctrl.index = (req, res) => {
+    res.send('Index Page')
+};
+ctrl.create = (req, res) => {
+    res.send('Index Page')
+};
+ctrl.like = (req, res) => {
+    res.send('Index Page')
+};
+ctrl.comment = (req, res) => {
+    res.send('Index Page')
+};
+ctrl.remove = (req, res) => {
+    res.send('Index Page')
+};
+
+
+module.exports = ctrl;
--- src/server/config.js
+++ src/server/config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+
+const routes = require('../routes/index');
+
+const errorHandler = require('errorhandler');
 module.exports = app => {
     // Settings
     app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, 'views'));
+    app.set('views', path.join(__dirname, '../views'));
     app.engine('.hbs', exphbs({
         defaultLayout: 'main',
         partialsDir: path.join(app.get('views'), 'partials'),
@@ -14,8 +21,23 @@ module.exports = app => {
     app.set('view engine', '.hbs');
 
     // Middlewares
-    
+    app.use(morgan('dev'));
+    app.use(multer({
+        dest: path.join(__dirname, '../public/upload/temp')
+    }).single('image'));
+    app.use(express.urlencoded({extended:false}));
+    app.use(express.json());
 
+    // Routes
+    routes(app);
+
+    // Statis Files
+    app.use('/public', express.static(path.join(__dirname, '../public')));
+
+    // ErrorHandler
+    if('development' === app.get('env')) {
+        app.use(errorHandler);
+    }
 
     return app;
 }
