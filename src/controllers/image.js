2e3279344f4de6f431746e39b97c6320276ea107
tonypera <paulurbian@hotmail.com>
2019-02-14 14:19:21 -0500

    upload image

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,10 +1,23 @@
+const path = require('path');
+const { randomString } = require('../helpers/libs');
+const fs = require('fs-extra');
 const ctrl = {};
 
 ctrl.index = (req, res) => {
     res.send('Index Page')
 };
-ctrl.create = (req, res) => {
-    res.send('Index Page')
+ctrl.create =  async (req, res) => {
+    const imgURL = randomString();
+    console.log(imgURL);
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve(`src/public/upload/${imgURL}${ext}`);
+
+    if (ext ==='.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        await fs.rename(imageTempPath, targetPath);
+    }
+
+    res.send('works!');
 };
 ctrl.like = (req, res) => {
     res.send('Index Page')
