36a95933ed7f934ad6e3c5be1e1a4500b0a1eab2
tonypera <paulurbian@hotmail.com>
2019-02-19 18:43:05 -0500

    update views

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -10,9 +10,20 @@ const { Image, Comment } = require('../models');
 const ctrl = {};
 
 ctrl.index = async (req, res) => {
+    const viewModel = { image: {}, comments: {} };
+
     const image = await Image.findOne({ filename: {$regex: req.params.image_id}});
-    const comments = await Comment.find({image_id: image._id});
-    res.render('image', {image, comments});
+    if (image) {
+        image.views = image.views + 1;
+        viewModel.image = image;
+        await image.save();
+        const comments = await Comment.find({image_id: image._id});
+        viewModel.comments = comments;
+        res.render('image', {image, comments});
+    } else {
+        res.redirect('/');
+    }
+    
 };
 ctrl.create = (req, res) => {
 
@@ -58,6 +69,8 @@ ctrl.comment = async (req, res) => {
         newComment.image_id = image._id;
         await newComment.save();
         res.redirect('/images/' + image.uniqueId);
+    } else {
+        res.redirect('/');
     }
 };
 ctrl.remove = (req, res) => {
